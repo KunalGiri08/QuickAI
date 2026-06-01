e4b5e05aa0a6fc2b500e214a01f7ae0884ec17ea
Kunal Giri <kunalgiri177@gmail.com>
2026-06-01 14:41:31 +0530

    Add routing and new pages for Home, Dashboard, BlogTitles, Community, GenerateImages, RemoveBackground, RemoveObjects, ReviewResume, and WriteArticle initial setup of pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Layout from './pages/Layout'
+import DashBoard from './pages/DashBoard'
+import WriteArticle from './pages/WriteArticle'
+import BlogTitles from './pages/BlogTitles'
+import Community from './pages/Community'
+import GenerateImages from './pages/GenerateImages'
+import RemoveBackground from './pages/RemoveBackground'
+import RemoveObjects from './pages/RemoveObjects'
+import ReviewResume from './pages/ReviewResume'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/ai' element={<Layout />}>
+          <Route index element={<DashBoard />} />
+          <Route path='write-article' element={<WriteArticle />} />
+          <Route path='blog-titles' element={<BlogTitles />} />
+          <Route path='community' element={<Community />} />
+          <Route path='generate-images' element={<GenerateImages />} />
+          <Route path='remove-background' element={<RemoveBackground />} />
+          <Route path='remove-objects' element={<RemoveObjects />} />
+          <Route path='review-resume' element={<ReviewResume />} />
+
+        </Route>
+      </Routes>
+    </div>
   )
 }
 
